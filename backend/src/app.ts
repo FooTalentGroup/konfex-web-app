631ec204e648df81ccda42bdeffe5897d2e141af
XCODE69 <maph0989@gmail.com>
2025-11-24 19:01:53 -0300

    hotfix: * enviar credentials desde el front

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,7 +13,6 @@ dotenv.config();
 const app: Express = express();
 
 app.use(corsMiddleware());
-app.options("*/", corsMiddleware());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(pinoHttp({ logger }));
