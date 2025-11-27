ab5676bc6b16e96e3daac5baf3036b690fb24236
XCODE69 <maph0989@gmail.com>
2025-11-27 18:06:57 -0300

    hotfix: agregar origen para solicitudes

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -8,6 +8,7 @@ const defaultOrigins = [
   "https://konfex-web-app-gilt.vercel.app",
   "https://konfex-web-app.vercel.app",
   "https://eos-konfex.onrender.com",
+  "https://konfex-web-app-2.onrender.com",
   "https://konfex-web-app-omega.vercel.app"
 ];
 
