5dde5fe72236646d88cc6ce447720e1d161ce2fe
XCODE69 <maph0989@gmail.com>
2025-11-28 16:19:01 -0300

    fix: * inicializar socket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,17 @@
-import { app } from './app';
+import { createServer } from "http";
+import { app } from "./app";
+import { initSocket } from "./config/socket";
 
+const PORT = process.env.PORT ?? 3001; // diferente del frontend
 
-const PORT = process.env.PORT ?? 3000;
+const server = createServer(app);
 
+// Inicializar Socket.IO
+initSocket(server);
 
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
-  console.log(
-    `📚 Documentación de la API: http://localhost:${PORT}/api/v1/docs`
-  );
+// Levantar el servidor
+server.listen(PORT, () => {
+  console.log(`🚀 Backend corriendo en http://localhost:${PORT}`);
+  console.log(`📚 Documentación de la API: http://localhost:${PORT}/api/v1/docs`);
   console.log(`🔍 Check de salud: http://localhost:${PORT}/api/v1/health`);
 });
