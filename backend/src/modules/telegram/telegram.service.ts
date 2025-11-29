56609932af56dadb5cb35235e0db79ee4334be54
Eliana Suancha Guzman <elianitasuanguz@gmail.com>
2025-11-29 16:03:34 -0500

    ajuste: arreglo de build

--- backend/src/modules/telegram/telegram.service.ts
+++ backend/src/modules/telegram/telegram.service.ts
@@ -74,22 +74,13 @@ export const getChatMessages = async (chatId: string | number) => {
   const messages = await telegramMessageRepository.findByChatId(chatId);
   
   return messages.map(
-    (message: {
-      id: any;
-      chatId: any;
-      text: string;
-      source: any;
-      firstName: string;
-      lastName: string;
-      username: any;
-      timestamp: any;
-    }) => ({
+    (message) => ({
       id: message.id,
       chatId: message.chatId,
       text: message.text.trim(),
       source: message.source,
-      firstName: message.firstName.trim(),
-      lastName: message.lastName.trim(),
+      firstName: message.firstName?.trim(),
+      lastName: message.lastName?.trim(),
       username: message.username,
       timestamp: message.timestamp,
     }),
