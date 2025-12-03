a040ff497ee943042cff65cc9b5fcf0adb87b1ea
Kevin Agustin Ruiz <Kevinagustinrockz@gmail.com>
2025-12-03 09:23:34 -0300

    updated prisma

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,20 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../generated/prisma/client";
+import { env } from "prisma/config";
 
-const prismaClientSingleton = () => {
+const adapter = () => {
   return new PrismaClient({
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "error", "warn"]
         : ["error"],
+    adapter: new PrismaPg(env("DATABASE_URL")),
+    errorFormat: "pretty",
   });
 };
 
 declare global {
   // eslint-disable-next-line no-var
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+  var prismaGlobal: undefined | ReturnType<typeof adapter>;
 }
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+const prisma = globalThis.prismaGlobal ?? adapter();
 
 export default prisma;
 
