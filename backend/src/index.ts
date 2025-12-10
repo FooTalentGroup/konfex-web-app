7273f9bfb82c5e57fc7a337f340c55697498f56a
Kevin Agustin Ruiz <Kevinagustinrockz@gmail.com>
2025-12-10 11:34:53 -0300

    fix build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import { createServer } from "http";
 
 import { app } from "./app";
 
-const PORT = process.env.PORT ?? 3001;
+const PORT = process.env.PORT || 3001;
 
 const server = createServer(app);
 
